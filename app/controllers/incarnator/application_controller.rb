e664963a02f10ac0a51c5cc78061300a713d9d0d
Andrey Sapozhnikov <skarm61@gmail.com>
2016-09-04 16:32:17 +0300

    Downgrade devise to 3.5.x

--- app/controllers/incarnator/application_controller.rb
+++ app/controllers/incarnator/application_controller.rb
@@ -3,7 +3,7 @@ module Incarnator
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name] )
+      devise_parameter_sanitizer.for(:sign_up) << :user_name
     end
   end
 end
